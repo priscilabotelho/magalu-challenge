2d0cec48f3c8f1160a6085bf8f2462f230d9b98b
Priscila Botelho <priscilafnbotelho@gmail.com>
2024-06-30 14:58:36 -0300

    aa

--- vpc.tf
+++ vpc.tf
@@ -3,16 +3,24 @@ provider "google" {
   region  = var.region
 }
 
+# Cria a rede VPC apenas se não existir
+resource "google_compute_network" "vpc" {
+  name                    = sensitive("${var.project_id}-vpc")
+  auto_create_subnetworks = false
+}
+
 # Verifica se a rede VPC já existe
 data "google_compute_network" "existing_vpc" {
   count = length(google_compute_network.vpc) > 0 ? 1 : 0
   name  = google_compute_network.vpc[0].name
 }
 
-# Cria a rede VPC apenas se não existir
-resource "google_compute_network" "vpc" {
-  name                    = sensitive("${var.project_id}-vpc")
-  auto_create_subnetworks = false
+# Cria a subrede apenas se não existir
+resource "google_compute_subnetwork" "subnet" {
+  name          = sensitive("${var.project_id}-subnet")
+  region        = var.region
+  network       = google_compute_network.vpc.name  # Referência direta ao nome da rede VPC
+  ip_cidr_range = "10.10.0.0/24"
 
   # Condicional para criar apenas se o recurso não existir
   count = length(data.google_compute_network.existing_vpc) == 0 ? 1 : 0
@@ -23,16 +31,5 @@ data "google_compute_subnetwork" "existing_subnet" {
   count   = length(google_compute_subnetwork.subnet) > 0 ? 1 : 0
   name    = google_compute_subnetwork.subnet[0].name
   region  = var.region
-  network = google_compute_network.vpc[0].name
-}
-
-# Cria a subrede apenas se não existir
-resource "google_compute_subnetwork" "subnet" {
-  name          = sensitive("${var.project_id}-subnet")
-  region        = var.region
-  network       = google_compute_network.vpc[0].name
-  ip_cidr_range = "10.10.0.0/24"
-
-  # Condicional para criar apenas se o recurso não existir
-  count = length(data.google_compute_subnetwork.existing_subnet) == 0 ? 1 : 0
+  network = google_compute_network.vpc.name  # Referência direta ao nome da rede VPC
 }
