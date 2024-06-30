a74960a19e6da38a6dc5245536d232ff30f285f6
Priscila Botelho <priscilafnbotelho@gmail.com>
2024-06-30 15:00:03 -0300

    s

--- vpc.tf
+++ vpc.tf
@@ -12,7 +12,7 @@ resource "google_compute_network" "vpc" {
 # Verifica se a rede VPC já existe
 data "google_compute_network" "existing_vpc" {
   count = length(google_compute_network.vpc) > 0 ? 1 : 0
-  name  = google_compute_network.vpc[0].name
+  name  = google_compute_network.vpc.name  # Referência direta ao nome da rede VPC
 }
 
 # Cria a subrede apenas se não existir
@@ -29,7 +29,7 @@ resource "google_compute_subnetwork" "subnet" {
 # Verifica se a subrede já existe
 data "google_compute_subnetwork" "existing_subnet" {
   count   = length(google_compute_subnetwork.subnet) > 0 ? 1 : 0
-  name    = google_compute_subnetwork.subnet[0].name
+  name    = google_compute_subnetwork.subnet.name  # Referência direta ao nome da subrede
   region  = var.region
   network = google_compute_network.vpc.name  # Referência direta ao nome da rede VPC
 }
