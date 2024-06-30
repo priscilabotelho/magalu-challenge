0d7f289bdc8e2cb681b994e3b31e60b1a9543816
Priscila Botelho <priscilafnbotelho@gmail.com>
2024-06-30 15:02:37 -0300

    ...

--- vpc.tf
+++ vpc.tf
@@ -3,33 +3,30 @@ provider "google" {
   region  = var.region
 }
 
-# Cria a rede VPC apenas se não existir
+# Verifica se a VPC já existe
+data "google_compute_network" "existing_vpc" {
+  name = "${var.project_id}-vpc"
+}
+
+# Cria a VPC apenas se não existir
 resource "google_compute_network" "vpc" {
+  count                   = length(data.google_compute_network.existing_vpc) == 0 ? 1 : 0
   name                    = sensitive("${var.project_id}-vpc")
   auto_create_subnetworks = false
 }
 
-# Verifica se a rede VPC já existe
-data "google_compute_network" "existing_vpc" {
-  count = length(google_compute_network.vpc) > 0 ? 1 : 0
-  name  = google_compute_network.vpc.name  # Referência direta ao nome da rede VPC
+# Verifica se a subnet já existe
+data "google_compute_subnetwork" "existing_subnet" {
+  name    = "${var.project_id}-subnet"
+  region  = var.region
+  network = google_compute_network.vpc[count.index].name
 }
 
-# Cria a subrede apenas se não existir
+# Cria a subnet apenas se não existir
 resource "google_compute_subnetwork" "subnet" {
+  count         = length(data.google_compute_subnetwork.existing_subnet) == 0 ? 1 : 0
   name          = sensitive("${var.project_id}-subnet")
   region        = var.region
-  network       = google_compute_network.vpc.name  # Referência direta ao nome da rede VPC
+  network       = google_compute_network.vpc[count.index].name
   ip_cidr_range = "10.10.0.0/24"
-
-  # Condicional para criar apenas se o recurso não existir
-  count = length(data.google_compute_network.existing_vpc) == 0 ? 1 : 0
-}
-
-# Verifica se a subrede já existe
-data "google_compute_subnetwork" "existing_subnet" {
-  count   = length(google_compute_subnetwork.subnet) > 0 ? 1 : 0
-  name    = google_compute_subnetwork.subnet.name  # Referência direta ao nome da subrede
-  region  = var.region
-  network = google_compute_network.vpc.name  # Referência direta ao nome da rede VPC
 }
