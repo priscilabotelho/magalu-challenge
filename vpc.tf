cb5f4baf0e2e1944807126a116a6569228ac46c3
Priscila Botelho <priscilafnbotelho@gmail.com>
2024-06-30 14:53:06 -0300

    atualizando vpc

--- vpc.tf
+++ vpc.tf
@@ -3,16 +3,34 @@ provider "google" {
   region  = var.region
 }
 
-# VPC
-resource "google_compute_network" "vpc" {
-  name                    = sensitive("${var.project_id}-vpc")
-  auto_create_subnetworks = "false"
+# Verifica se a rede VPC já existe
+data "google_compute_network" "existing_vpc" {
+  name = sensitive("${var.project_id}-vpc")
 }
 
-# Subnet
+# Cria a rede VPC apenas se não existir
+resource "google_compute_network" "vpc" {
+  name                    = sensitive("${var.project_id}-vpc")
+  auto_create_subnetworks = false
+
+  # Condicional para criar apenas se o recurso não existir
+  count = length(data.google_compute_network.existing_vpc) == 0 ? 1 : 0
+}
+
+# Verifica se a subrede já existe
+data "google_compute_subnetwork" "existing_subnet" {
+  name    = sensitive("${var.project_id}-subnet")
+  region  = var.region
+  network = google_compute_network.vpc.name
+}
+
+# Cria a subrede apenas se não existir
 resource "google_compute_subnetwork" "subnet" {
   name          = sensitive("${var.project_id}-subnet")
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
+
+  # Condicional para criar apenas se o recurso não existir
+  count = length(data.google_compute_subnetwork.existing_subnet) == 0 ? 1 : 0
 }
