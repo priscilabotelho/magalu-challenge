88a61f81c09b327076f3eed97bba31f6d28f36d4
Priscila Botelho <priscilafnbotelho@gmail.com>
2024-06-30 15:06:05 -0300

    aaaaa

--- vpc.tf
+++ vpc.tf
@@ -3,16 +3,28 @@ provider "google" {
   region  = var.region
 }
 
-# VPC
+# Verifica se a VPC já existe
+data "google_compute_network" "existing_vpc" {
+  name = "${var.project_id}-vpc"
+}
+
+# Cria a VPC apenas se não existir
 resource "google_compute_network" "vpc" {
   name                    = sensitive("${var.project_id}-vpc")
   auto_create_subnetworks = false
+
+  lifecycle {
+    ignore_changes = [auto_create_subnetworks]
+  }
+
+  # Usa o count apenas se a VPC ainda não existir
+  count = length(data.google_compute_network.existing_vpc) == 0 ? 1 : 0
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
   name          = sensitive("${var.project_id}-subnet")
   region        = var.region
-  network       = google_compute_network.vpc.self_link  # Referencia a VPC criada ou existente
+  network       = google_compute_network.vpc[0].self_link  # Referencia a VPC criada ou existente
   ip_cidr_range = "10.10.0.0/24"
 }
