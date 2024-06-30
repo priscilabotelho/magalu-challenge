bc1d3b3cdef334dc214970dc1a7f81a941ba7bd7
Priscila Botelho <priscilafnbotelho@gmail.com>
2024-06-30 14:04:43 -0300

    alterando tudo

--- gke.tf
+++ gke.tf
@@ -1,44 +1,36 @@
-provider "google" {
-  project = var.project
-  region  = var.region
-}
-
-resource "google_container_cluster" "magalu_challenge" {
-  name     = var.cluster_name
+# Cluster GKE
+resource "google_container_cluster" "magalu_cluster" {
+  name     = sensitive("${var.project_id}-gke")
   location = var.region
 
-  initial_node_count = 1
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
-  node_config {
-    machine_type = "e2-medium"
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append",
-    ]
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
   }
-}
 
-resource "google_container_node_pool" "primary_nodes" {
-  cluster    = google_container_cluster.magalu_challenge.name  # Alterado para google_container_cluster.magalu_challenge
-  location   = google_container_cluster.magalu_challenge.location
-  node_count = var.node_count
+# Node Pool Gerenciado Separadamente
+resource "google_container_node_pool" "nodes_primarios" {
+  name       = "${google_container_cluster.magalu_cluster.name}-node-pool"
+  location   = var.region
+  cluster    = google_container_cluster.magalu_cluster.name
+  node_count = var.gke_num_nodes
 
   node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-
     oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append",
     ]
+
+    labels = {
+      env = sensitive(var.project_id)
+    }
+
+    machine_type = "e2-medium"
+    tags         = ["gke-node", sensitive("${var.project_id}-gke")]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,14 @@
-variable "project" {
-  description = "The project ID to deploy to"
-  default     = "magalu-challenge"
+variable "gke_num_nodes" {
+  default     = 2
+  description = "numero de nodes para o cluster"
+}
+
+variable "project_id" {
+  description = "project id"
+  default =   "magalu-challenge"
 }
 
 variable "region" {
-  description = "The region to deploy to"
-  default     = "us-central1"
-}
-
-variable "cluster_name" {
-  description = "The name of the Kubernetes cluster"
-  default     = "magalu-cluster"
-}
-
-variable "node_count" {
-  description = "The number of nodes in the Kubernetes cluster"
-  default     = 1
+  description = "region"
+  default =   "us-central1"
 }
